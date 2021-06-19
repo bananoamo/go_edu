c714d87b1fedcf3f2f92e37c8f304d6a372e1542
Alexander <unitestgroup@yandex.ru>
2021-06-19 14:17:27 +0400

    finished switch.go

---
+++ go_by_example/switch.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+import "time"
+
+func main() {
+
+	var i int = 2
+
+	switch i {
+		case 1:
+			fmt.Println("One")
+		case 2:
+			fmt.Println("Two")
+		case 3:
+			fmt.Println("Three")
+		case 4:
+			fmt.Println("Four")
+	}
+}
