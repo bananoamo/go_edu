016a7c3b35e817d67ec40a2944c070654a5ee4b0
Alexander <unitestgroup@yandex.ru>
2021-10-02 12:17:59 +0400

    added text file for tests and made fixes

--- guess_separator/guess_separator.go
+++ guess_separator/guess_separator.go
@@ -19,10 +19,7 @@ func main() {
 	numberOfReadLines, lines := readFileUpToNLines(os.Args[1], 5)
 	separatorsByLines := getSepListFromLines(lines, separators, numberOfReadLines)
 	lineSeparator := guessSep(separators, separatorsByLines, numberOfReadLines)
-	//printResult(lineSeparator)
-	fmt.Printf("%#q\n", []rune(lineSeparator))
-	fmt.Println(separatorsByLines)
-
+	printResult(lineSeparator)
 }
 
 func readFileUpToNLines(fileName string, maxLines int) (int, []string) {
@@ -33,18 +30,20 @@ func readFileUpToNLines(fileName string, maxLines int) (int, []string) {
 	}
 	defer file.Close()
 	rd := bufio.NewReader(file)
-	lines := make([]string, 0, maxLines)
+	lines := make([]string, maxLines)
 	i := 0
 	for ; i < maxLines; i++ {
 		line, err := rd.ReadString('\n')
+		if line != "" {
+			lines[i] = line
+		}
 		if err != nil {
 			if err == io.EOF {
 				break
 			}
 		}
-		lines = append(lines, line)
 	}
-	return i, lines
+	return i, lines[:i]
 }
 func getSepListFromLines(lines, sep []string, maxLines int) [][]int {
 	sepList := make([][]int, len(sep))
@@ -56,11 +55,11 @@ func getSepListFromLines(lines, sep []string, maxLines int) [][]int {
 	}
 	return sepList
 }
-func guessSep(sepList []string, sepByLines [][]int, maxline int) string {
+func guessSep(sepList []string, sepByLines [][]int, maxLine int) string {
 	for sepIdx := range sepList {
 		same := true
 		target := sepByLines[sepIdx][0]
-		for lineIdx := 1; lineIdx < maxline; lineIdx++ {
+		for lineIdx := 1; lineIdx < maxLine; lineIdx++ {
 			if sepByLines[sepIdx][lineIdx] != target {
 				same = false
 				break
@@ -71,4 +70,14 @@ func guessSep(sepList []string, sepByLines [][]int, maxline int) string {
 		}
 	}
 	return ""
+}
+func printResult(separator string) {
+	switch separator {
+	case "\t":
+		fmt.Printf("tab-separated\n")
+	case "":
+		fmt.Printf("white-space separated or not separated at all\n")
+	default:
+		fmt.Printf("%s-separated\n", separator)
+	}
 }
